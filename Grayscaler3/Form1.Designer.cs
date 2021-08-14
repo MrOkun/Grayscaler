ca97ebffa042d089b5aece7dcb977ea3773c01ad
MrOkun <74727162+MrOkun@users.noreply.github.com>
2021-08-14 15:29:04 +0300

    Main Files

---
+++ Grayscaler3/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Grayscaler3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Original_Image = new System.Windows.Forms.PictureBox();
+            this.Modified_Image = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Image_Selector = new System.Windows.Forms.ComboBox();
+            this.Method_Selector = new System.Windows.Forms.ComboBox();
+            this.Load_Image = new System.Windows.Forms.Button();
+            this.Save_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Original_Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Modified_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Original_Image
+            // 
+            this.Original_Image.Location = new System.Drawing.Point(12, 34);
+            this.Original_Image.Name = "Original_Image";
+            this.Original_Image.Size = new System.Drawing.Size(165, 165);
+            this.Original_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Original_Image.TabIndex = 1;
+            this.Original_Image.TabStop = false;
+            // 
+            // Modified_Image
+            // 
+            this.Modified_Image.Location = new System.Drawing.Point(187, 34);
+            this.Modified_Image.Name = "Modified_Image";
+            this.Modified_Image.Size = new System.Drawing.Size(165, 165);
+            this.Modified_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Modified_Image.TabIndex = 2;
+            this.Modified_Image.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(184, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Modified :";
+            // 
+            // Image_Selector
+            // 
+            this.Image_Selector.FormattingEnabled = true;
+            this.Image_Selector.Location = new System.Drawing.Point(12, 205);
+            this.Image_Selector.Name = "Image_Selector";
+            this.Image_Selector.Size = new System.Drawing.Size(165, 21);
+            this.Image_Selector.TabIndex = 5;
+            this.Image_Selector.SelectedIndexChanged += new System.EventHandler(this.Image_Selector_Changed);
+            // 
+            // Method_Selector
+            // 
+            this.Method_Selector.FormattingEnabled = true;
+            this.Method_Selector.Items.AddRange(new object[] {
+            "All Channels (Original)",
+            "By Red channel (Uncorrect)",
+            "By Green channel (Uncorrect)",
+            "By Blue channel (Uncorrect)",
+            "Average",
+            "Luminance (Photoshop)",
+            "Desaturation"});
+            this.Method_Selector.Location = new System.Drawing.Point(187, 205);
+            this.Method_Selector.Name = "Method_Selector";
+            this.Method_Selector.Size = new System.Drawing.Size(165, 21);
+            this.Method_Selector.TabIndex = 6;
+            this.Method_Selector.SelectedIndexChanged += new System.EventHandler(this.Method_Selector_Changed);
+            // 
+            // Load_Image
+            // 
+            this.Load_Image.Location = new System.Drawing.Point(12, 232);
+            this.Load_Image.Name = "Load_Image";
+            this.Load_Image.Size = new System.Drawing.Size(165, 32);
+            this.Load_Image.TabIndex = 7;
+            this.Load_Image.Text = "Load Image";
+            this.Load_Image.UseVisualStyleBackColor = true;
+            this.Load_Image.Click += new System.EventHandler(this.Load_Image_Click);
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(12, 270);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(165, 32);
+            this.Save_Button.TabIndex = 8;
+            this.Save_Button.Text = "Save Image";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 311);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.Load_Image);
+            this.Controls.Add(this.Method_Selector);
+            this.Controls.Add(this.Image_Selector);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Modified_Image);
+            this.Controls.Add(this.Original_Image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form_Sized);
+            ((System.ComponentModel.ISupportInitialize)(this.Original_Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Modified_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Original_Image;
+        private System.Windows.Forms.PictureBox Modified_Image;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Image_Selector;
+        private System.Windows.Forms.ComboBox Method_Selector;
+        private System.Windows.Forms.Button Load_Image;
+        private System.Windows.Forms.Button Save_Button;
+    }
+}
+
---
+++ Grayscaler3/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grayscaler3
+{
+    public partial class Form1 : Form
+    {
+        private string _path = $@"{Directory.GetCurrentDirectory()}\Img\coffie.jpg";
+        private string _standartFilePath;
+        private Bitmap _modifiedImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ImageDownload();
+            Render();
+        }
+
+        private void Image_Selector_Changed(object sender, EventArgs e)
+        {
+            _path = Image_Selector.SelectedItem.ToString();
+            Render();
+        }
+
+        private void Method_Selector_Changed(object sender, EventArgs e)
+        {
+            Render();
+        }
+
+        private void Render()
+        {
+            _modifiedImage = new Bitmap(_path, true);
+            Modified_Image.Image = new Bitmap(_path, true);
+            Original_Image.Image = new Bitmap(_path, true);
+
+            int x, y;
+
+            for (x = 0; x < _modifiedImage.Width; x++)
+            {
+                for (y = 0; y < _modifiedImage.Height; y++)
+                {
+                    Color pixelColor = _modifiedImage.GetPixel(x, y);
+
+                    Color newColor = Algarithm(Method_Selector.SelectedIndex, pixelColor);
+                    _modifiedImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            Modified_Image.Image = _modifiedImage;
+        }
+
+        private Color Algarithm(int way, Color pixelColor)
+        {
+            switch (way) 
+            {
+                default: //All channels.
+                    {
+                        return Color.FromArgb(pixelColor.R, pixelColor.G, pixelColor.B);
+                    }
+                case 0: //All channels.
+                    {
+                        return Color.FromArgb(pixelColor.R, pixelColor.G, pixelColor.B);
+                    }
+                case 1: //By Red channel.
+                    {
+                        return Color.FromArgb(pixelColor.R, pixelColor.R, pixelColor.R);
+                    }
+                case 2: //By Green channel.
+                    {
+                        return Color.FromArgb(pixelColor.G, pixelColor.G, pixelColor.G);
+                    }
+                case 3: //By Blue channel.
+                    {
+                        return Color.FromArgb(pixelColor.B, pixelColor.B, pixelColor.B);
+                    }
+                case 4: //Average
+                    {
+                        var grayscaleColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                        return Color.FromArgb(grayscaleColor, grayscaleColor, grayscaleColor);
+                    }
+                case 5: //Luminance (Photoshop)
+                    {
+                        var grayscaleColor = 0.299 * pixelColor.R + 0.587 * pixelColor.G + 0.114 * pixelColor.B;
+                        grayscaleColor = Math.Ceiling(grayscaleColor);
+
+                        return Color.FromArgb((int)grayscaleColor, (int)grayscaleColor, (int)grayscaleColor);
+                    }
+                case 6: //Desaturation
+                    {
+                        int Max = Math.Max(Math.Max(pixelColor.R, pixelColor.G), pixelColor.B);
+                        int Min = Math.Min(Math.Min(pixelColor.R, pixelColor.G), pixelColor.B);
+
+                        var grayscaleColor = (Max + Min) / 2;
+
+                        return Color.FromArgb(grayscaleColor, grayscaleColor, grayscaleColor);
+                    }
+            }
+
+        }
+
+        private void ImageDownload()
+        {
+            WebClient webClient = new WebClient();
+
+            var coffieUrl = "https://raw.githubusercontent.com/MrOkun/files/main/coffie.jpg";
+            var gamblUrl = "https://raw.githubusercontent.com/MrOkun/files/main/gambl.jpg";
+            var coffiePath = $"{Directory.GetCurrentDirectory()}/Img/coffie.jpg";
+            var gamblPath = $"{Directory.GetCurrentDirectory()}/Img/gambl.jpg";
+
+            if (!Directory.Exists("Img"))
+            {
+                Directory.CreateDirectory("Img");
+            }
+            if (!File.Exists($@"{Directory.GetCurrentDirectory()}\Img\gambl.jpg"))
+            {
+                webClient.DownloadFile(gamblUrl, gamblPath);
+            }
+            if (!File.Exists($@"{Directory.GetCurrentDirectory()}\Img\coffie.jpg"))
+            {
+                webClient.DownloadFile(coffieUrl, coffiePath);
+            }
+
+            Image_Selector.Items.Add(coffiePath);
+            Image_Selector.Items.Add(gamblPath);
+        }
+
+        private void Form_Sized(object sender, EventArgs e)
+        {
+            this.Height = 380;
+            this.Width = 350;
+        }
+
+        private void Load_Image_Click(object sender, EventArgs e)
+        {
+            var OPF = new OpenFileDialog();
+            OPF.Filter = "Файлы .png|*.png|Файлы .jpg|*.jpg";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                _path = OPF.FileName;
+                Render();
+                Image_Selector.Items.Add(OPF.FileName);
+            }
+            else
+            {
+                OPF.Dispose();
+            }
+        }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+            _standartFilePath = $"{Directory.GetCurrentDirectory()}\\Img\\ModifiedImage.png";
+
+            cheak:
+            if (File.Exists(_standartFilePath))
+            {
+                _standartFilePath = _standartFilePath.Replace(".png", null);
+                _standartFilePath = _standartFilePath + "New" + ".png";
+                goto cheak;
+            }
+
+            Modified_Image.Image.Save(_standartFilePath);
+            MessageBox.Show($"Image saved in {_standartFilePath}");
+        }
+    }
+}
